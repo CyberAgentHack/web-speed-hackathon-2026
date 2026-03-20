e02920b8f8d66a781e47625cfad2a4f677b7a65f
cp-20 <n.naoki.1587@gmail.com>
2026-03-20 22:56:13 +0900

    🫓 redux

--- application/client/src/store/index.ts
+++
@@ -1,8 +0,0 @@
-import { legacy_createStore as createStore, Dispatch, UnknownAction } from "redux";
-
-const rootReducer = (state = {}) => state;
-
-export type RootState = ReturnType<typeof rootReducer>;
-export type AppDispatch = Dispatch<UnknownAction>;
-
-export const store = createStore(rootReducer);
