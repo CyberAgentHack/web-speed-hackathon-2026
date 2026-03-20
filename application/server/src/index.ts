a38c371997228036b67e2e4c576224a35d4d258c
sandambata.kei <keisan_333@icloud.com>
2026-03-20 14:47:22 +0900

    fix: Crokキャッシュの初期化を追加し、提案をメモリにキャッシュするように変更

--- application/server/src/index.ts
+++ application/server/src/index.ts
@@ -2,9 +2,11 @@ import "@web-speed-hackathon-2026/server/src/utils/express_websocket_support";
 import { app } from "@web-speed-hackathon-2026/server/src/app";
 
 import { initializeSequelize } from "./sequelize";
+import { initializeCrokCache } from "@web-speed-hackathon-2026/server/src/routes/api/crok";
 
 async function main() {
   await initializeSequelize();
+  await initializeCrokCache();
 
   const server = app.listen(Number(process.env["PORT"] || 3000), "0.0.0.0", () => {
     const address = server.address();
