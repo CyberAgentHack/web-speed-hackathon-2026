aa1dfb0712083adc4c43ddbb9668359a81c52f6f
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-03-20 12:53:28 +0900

    Remove jquery

--- application/client/src/containers/AuthModalContainer.tsx
+++ application/client/src/containers/AuthModalContainer.tsx
@@ -16,8 +16,7 @@ const ERROR_MESSAGES: Record<string, string> = {
   USERNAME_TAKEN: "ユーザー名が使われています",
 };
 
-function getErrorCode(err: JQuery.jqXHR<unknown>, type: "signin" | "signup"): string {
-  const responseJSON = err.responseJSON;
+function getErrorCode(responseJSON: unknown, type: "signin" | "signup"): string {
   if (
     typeof responseJSON !== "object" ||
     responseJSON === null ||
@@ -68,7 +67,7 @@ export const AuthModalContainer = ({ id, onUpdateActiveUser }: Props) => {
         }
         handleRequestCloseModal();
       } catch (err: unknown) {
-        const error = getErrorCode(err as JQuery.jqXHR<unknown>, values.type);
+        const error = getErrorCode(err, values.type);
         throw new SubmissionError({
           _error: error,
         });
--- application/client/src/utils/fetchers.ts
+++ application/client/src/utils/fetchers.ts
@@ -1,58 +1,47 @@
-import $ from "jquery";
-import { gzip } from "pako";
-
 export async function fetchBinary(url: string): Promise<ArrayBuffer> {
-  const result = await $.ajax({
-    async: false,
-    dataType: "binary",
-    method: "GET",
-    responseType: "arraybuffer",
-    url,
-  });
-  return result;
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw await res.json();
+  }
+  return await res.arrayBuffer();
 }
 
 export async function fetchJSON<T>(url: string): Promise<T> {
-  const result = await $.ajax({
-    async: false,
-    dataType: "json",
-    method: "GET",
-    url,
-  });
-  return result;
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw await res.json();
+  }
+  return await res.json();
 }
 
 export async function sendFile<T>(url: string, file: File): Promise<T> {
-  const result = await $.ajax({
-    async: false,
-    data: file,
-    dataType: "json",
+  const res = await fetch(url, {
+    method: "POST",
     headers: {
       "Content-Type": "application/octet-stream",
     },
-    method: "POST",
-    processData: false,
-    url,
+    body: new Blob([file]),
   });
-  return result;
+  if (!res.ok) {
+    throw await res.json();
+  }
+  return await res.json();
 }
 
 export async function sendJSON<T>(url: string, data: object): Promise<T> {
   const jsonString = JSON.stringify(data);
-  const uint8Array = new TextEncoder().encode(jsonString);
-  const compressed = gzip(uint8Array);
+  const stream = new Blob([jsonString]).stream().pipeThrough(new CompressionStream("gzip"));
 
-  const result = await $.ajax({
-    async: false,
-    data: compressed,
-    dataType: "json",
+  const res = await fetch(url, {
+    method: "POST",
     headers: {
       "Content-Encoding": "gzip",
       "Content-Type": "application/json",
     },
-    method: "POST",
-    processData: false,
-    url,
+    body: stream,
   });
-  return result;
+  if (!res.ok) {
+    throw await res.json();
+  }
+  return await res.json();
 }
