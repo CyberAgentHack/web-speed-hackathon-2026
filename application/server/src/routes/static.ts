4c600a96c9b70fe7189eb272ad72beb07ca745cf
kitsne241 <169665926+kitsne241@users.noreply.github.com>
2026-03-20 16:54:06 +0900

    画像を直srcで読み込み & alt を API で取得

--- application/server/src/routes/static.ts
+++ application/server/src/routes/static.ts
@@ -2,34 +2,16 @@ import history from "connect-history-api-fallback";
 import { Router } from "express";
 import serveStatic from "serve-static";
 
-import {
-  CLIENT_DIST_PATH,
-  PUBLIC_PATH,
-  UPLOAD_PATH,
-} from "@web-speed-hackathon-2026/server/src/paths";
+import { CLIENT_DIST_PATH, PUBLIC_PATH, UPLOAD_PATH } from "@web-speed-hackathon-2026/server/src/paths";
 
 export const staticRouter = Router();
 
-// SPA 対応のため、ファイルが存在しないときに index.html を返す
+// 拡張子がついてるリクエストは index.html に書き換えて CLIENT_DIST_PATH に引っかける
+// ついてないリクエストはそのまま通して他のルーターに引っかける
 staticRouter.use(history());
 
-staticRouter.use(
-  serveStatic(UPLOAD_PATH, {
-    etag: false,
-    lastModified: false,
-  }),
-);
+staticRouter.use(serveStatic(UPLOAD_PATH));
 
-staticRouter.use(
-  serveStatic(PUBLIC_PATH, {
-    etag: false,
-    lastModified: false,
-  }),
-);
+staticRouter.use(serveStatic(PUBLIC_PATH));
 
-staticRouter.use(
-  serveStatic(CLIENT_DIST_PATH, {
-    etag: false,
-    lastModified: false,
-  }),
-);
+staticRouter.use(serveStatic(CLIENT_DIST_PATH));
