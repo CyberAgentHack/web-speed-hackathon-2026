751123a6954ef133adc9f0c6ab3eb16c3a8cd51c
nyawach <yuunyawach@gmail.com>
2026-03-20 11:36:21 +0900

    fix: replace with fetch API

--- application/client/webpack.config.js
+++ application/client/webpack.config.js
@@ -32,7 +32,6 @@ const config = {
     main: [
       "core-js",
       "regenerator-runtime/runtime",
-      "jquery-binarytransport",
       path.resolve(SRC_PATH, "./index.css"),
       path.resolve(SRC_PATH, "./buildinfo.ts"),
       path.resolve(SRC_PATH, "./index.tsx"),
@@ -70,10 +69,8 @@ const config = {
   },
   plugins: [
     new webpack.ProvidePlugin({
-      $: "jquery",
       AudioContext: ["standardized-audio-context", "AudioContext"],
       Buffer: ["buffer", "Buffer"],
-      "window.jQuery": "jquery",
     }),
     new webpack.EnvironmentPlugin({
       BUILD_DATE: new Date().toISOString(),
