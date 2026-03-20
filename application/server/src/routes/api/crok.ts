eda067404c822e5e2067cfd0601beb26cd820ad9
Takayuki Ueno <mail@kichi2004.jp>
2026-03-20 14:21:53 +0900

    キャッシュ設定の変更

--- application/server/src/routes/api/crok.ts
+++ application/server/src/routes/api/crok.ts
@@ -27,7 +27,6 @@ crokRouter.get("/crok", async (req, res) => {
   }
 
   res.setHeader("Content-Type", "text/event-stream");
-  res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
   res.flushHeaders();
 
