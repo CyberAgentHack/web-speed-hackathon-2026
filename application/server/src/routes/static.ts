78f4f0c2f97b769fb3a293a260e43b7085a8a313
cp-20 <n.naoki.1587@gmail.com>
2026-03-20 10:58:05 +0900

    static files を改善

--- application/server/src/routes/static.ts
+++ application/server/src/routes/static.ts
@@ -13,23 +13,6 @@ export const staticRouter = Router();
 // SPA 対応のため、ファイルが存在しないときに index.html を返す
 staticRouter.use(history());
 
-staticRouter.use(
-  serveStatic(UPLOAD_PATH, {
-    etag: false,
-    lastModified: false,
-  }),
-);
-
-staticRouter.use(
-  serveStatic(PUBLIC_PATH, {
-    etag: false,
-    lastModified: false,
-  }),
-);
-
-staticRouter.use(
-  serveStatic(CLIENT_DIST_PATH, {
-    etag: false,
-    lastModified: false,
-  }),
-);
+staticRouter.use(serveStatic(UPLOAD_PATH));
+staticRouter.use(serveStatic(PUBLIC_PATH));
+staticRouter.use(serveStatic(CLIENT_DIST_PATH));
