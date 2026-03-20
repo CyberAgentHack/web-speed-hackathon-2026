af994c013d22b032765ac828ad8167b9f0fd2e83
asa1984 <satoasa9913@gmail.com>
2026-03-21 00:09:16 +0900

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              brotli
+              python314Packages.fonttools
+              python314Packages.brotli
+            ];
+          };
+        };
+    };
+}
