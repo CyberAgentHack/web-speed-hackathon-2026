c4b68fb292bd80a75db043a7b60b0a6c1587c7fb
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-03-20 13:00:06 +0900

    Add bundle analyzer

--- application/client/vite.config.ts
+++ application/client/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import babel from "vite-plugin-babel";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { analyzer } from "vite-bundle-analyzer";
 
 export default defineConfig({
-  plugins: [nodePolyfills(), babel()],
+  plugins: [nodePolyfills(), babel(), analyzer()],
   build: {
     outDir: "../dist",
     emptyOutDir: true,
