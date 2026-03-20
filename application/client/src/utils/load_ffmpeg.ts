f692f99d6ff69b1ada698aa5d421ccd2c459682f
yas-ako <105139975+yas-ako@users.noreply.github.com>
2026-03-20 13:04:53 +0900

    バイナリをバンドルに含めないように

--- application/client/src/utils/load_ffmpeg.ts
+++ application/client/src/utils/load_ffmpeg.ts
@@ -1,16 +1,11 @@
 import { FFmpeg } from "@ffmpeg/ffmpeg";
+import coreURL from "@ffmpeg/core?binary";
+import wasmURL from "@ffmpeg/core/wasm?binary";
 
 export async function loadFFmpeg(): Promise<FFmpeg> {
   const ffmpeg = new FFmpeg();
 
-  await ffmpeg.load({
-    coreURL: await import("@ffmpeg/core?binary").then(({ default: b }) => {
-      return URL.createObjectURL(new Blob([b], { type: "text/javascript" }));
-    }),
-    wasmURL: await import("@ffmpeg/core/wasm?binary").then(({ default: b }) => {
-      return URL.createObjectURL(new Blob([b], { type: "application/wasm" }));
-    }),
-  });
+  await ffmpeg.load({ coreURL, wasmURL });
 
   return ffmpeg;
 }
