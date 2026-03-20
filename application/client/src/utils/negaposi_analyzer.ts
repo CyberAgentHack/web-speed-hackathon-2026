e9a7f583ec05ffa698d3dab09e65f445e82ca88d
kavos113 <kohei.hadano@gmail.com>
2026-03-20 17:25:08 +0900

    dynamic

--- application/client/src/utils/negaposi_analyzer.ts
+++ application/client/src/utils/negaposi_analyzer.ts
@@ -1,9 +1,14 @@
-import Bluebird from "bluebird";
-import kuromoji, { type Tokenizer, type IpadicFeatures } from "kuromoji";
-import analyze from "negaposi-analyzer-ja";
+import { type Tokenizer, type IpadicFeatures } from "kuromoji";
 
 async function getTokenizer(): Promise<Tokenizer<IpadicFeatures>> {
-  const builder = Bluebird.promisifyAll(kuromoji.builder({ dicPath: "/dicts" }));
+  const [kuromoji, Bluebird] = await Promise.all([
+    import("kuromoji"),
+    import("bluebird"),
+  ]);
+
+  const builder = Bluebird.default.promisifyAll(
+    kuromoji.builder({ dicPath: "/dicts" }),
+  );
   return await builder.buildAsync();
 }
 
@@ -13,10 +18,12 @@ type SentimentResult = {
 };
 
 export async function analyzeSentiment(text: string): Promise<SentimentResult> {
+  const analyze = await import("negaposi-analyzer-ja");
+
   const tokenizer = await getTokenizer();
   const tokens = tokenizer.tokenize(text);
 
-  const score = analyze(tokens);
+  const score = analyze.default(tokens);
 
   let label: SentimentResult["label"];
   if (score > 0.1) {
