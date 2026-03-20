bd81cd3b519e3599b42af5c45fa917fc20afe5c7
nyawach <yuunyawach@gmail.com>
2026-03-21 01:15:06 +0900

    fix: add width and height

--- application/client/src/components/direct_message/DirectMessageListPage.tsx
+++ application/client/src/components/direct_message/DirectMessageListPage.tsx
@@ -93,7 +93,9 @@ const ConversationItem = ({ conversation, activeUser }: { conversation: Models.D
           <img
             alt={peer.profileImage.alt}
             className="w-12 shrink-0 self-start rounded-full"
+            height="48"
             src={getProfileImagePath(peer.profileImage.id)}
+            width="48"
           />
           <div className="flex flex-1 flex-col">
             <div className="flex items-center justify-between">
