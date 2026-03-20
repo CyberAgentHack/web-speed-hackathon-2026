f685b418ad9eacb76dd9c19886ba666f1bd66dc3
Shunto Ishiguro <shunto.ishiguro.job@gmail.com>
2026-03-20 14:13:22 +0900

    fix: fetchJSONで非200レスポンス時にエラーをthrowするよう修正

--- application/client/src/utils/fetchers.ts
+++ application/client/src/utils/fetchers.ts
@@ -7,6 +7,9 @@ export async function fetchBinary(url: string): Promise<ArrayBuffer> {
 
 export async function fetchJSON<T>(url: string): Promise<T> {
   const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`HTTP ${res.status}`);
+  }
   return res.json() as Promise<T>;
 }
 
