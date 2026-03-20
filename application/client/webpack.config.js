0ee289c60364e09414c684b43b417a653a33a24c
honokakimura <2495090t@stu.kobe-u.ac.jp>
2026-03-20 17:04:30 +0900

    fix: devtool 設定を無効化し、エントリーポイントから不要な依存関係を削除

--- application/client/webpack.config.js
+++ application/client/webpack.config.js
@@ -25,11 +25,9 @@ const config = {
     ],
     static: [PUBLIC_PATH, UPLOAD_PATH],
   },
-  devtool: "inline-source-map",
+  devtool: false,
   entry: {
     main: [
-      "core-js",
-      "regenerator-runtime/runtime",
       "jquery-binarytransport",
       path.resolve(SRC_PATH, "./index.css"),
       path.resolve(SRC_PATH, "./buildinfo.ts"),
