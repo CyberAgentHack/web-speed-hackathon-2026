72cde94a941f338921c2ce4f697fb55e3a96eae7
nyawach <yuunyawach@gmail.com>
2026-03-21 01:04:22 +0900

    feat: move font css into term screen

--- application/client/src/containers/TermContainer.tsx
+++ application/client/src/containers/TermContainer.tsx
@@ -1,3 +1,5 @@
+import "@web-speed-hackathon-2026/client/src/components/term/term_fonts.css";
+
 import { TermPage } from "@web-speed-hackathon-2026/client/src/components/term/TermPage";
 
 export const TermContainer = () => {
