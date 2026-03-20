30d38b43e400b9cc40249df73c2bfe902a48d3f8
nyawach <yuunyawach@gmail.com>
2026-03-20 11:55:45 +0900

    fix: create base fetcher to adapt status code

--- application/client/src/utils/fetchers.ts
+++ application/client/src/utils/fetchers.ts
@@ -1,15 +1,23 @@
+async function baseFetcher(url: string, init?: RequestInit): Promise<Response> {
+  const response = await fetch(url, init);
+  if (!response.ok) {
+    throw new Error(`${response.status} ${response.statusText}`);
+  }
+  return response;
+}
+
 export async function fetchBinary(url: string): Promise<ArrayBuffer> {
-  const response = await fetch(url);
+  const response = await baseFetcher(url);
   return response.arrayBuffer();
 }
 
 export async function fetchJSON<T>(url: string): Promise<T> {
-  const response = await fetch(url);
+  const response = await baseFetcher(url);
   return response.json() as Promise<T>;
 }
 
 export async function sendFile<T>(url: string, file: File): Promise<T> {
-  const response = await fetch(url, {
+  const response = await baseFetcher(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/octet-stream",
@@ -20,7 +28,7 @@ export async function sendFile<T>(url: string, file: File): Promise<T> {
 }
 
 export async function sendJSON<T>(url: string, data: object): Promise<T> {
-  const response = await fetch(url, {
+  const response = await baseFetcher(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
