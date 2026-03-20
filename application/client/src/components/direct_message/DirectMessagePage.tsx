f1d859783ea59c2d7547438d64e1b45ba3945e98
tkhs0813 <takahashiry.0813@gmail.com>
2026-03-20 18:33:22 +0900

    ポーリングをやめてResizeObserverに移行

--- application/client/src/components/direct_message/DirectMessagePage.tsx
+++ application/client/src/components/direct_message/DirectMessagePage.tsx
@@ -74,15 +74,17 @@ export const DirectMessagePage = ({
   );
 
   useEffect(() => {
-    const id = setInterval(() => {
-      const height = Number(window.getComputedStyle(document.body).height.replace("px", ""));
-      if (height !== scrollHeightRef.current) {
-        scrollHeightRef.current = height;
-        window.scrollTo(0, height);
+    const observer = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        const height = entry.contentRect.height;
+        if (height !== scrollHeightRef.current) {
+          scrollHeightRef.current = height;
+          window.scrollTo(0, height);
+        }
       }
-    }, 1);
-
-    return () => clearInterval(id);
+    });
+    observer.observe(document.body);
+    return () => observer.disconnect();
   }, []);
 
   if (conversationError != null) {
