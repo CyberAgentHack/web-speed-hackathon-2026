1d1d059b6c5da6d21c9221334824485b89401e25
MATSUU Takuto <matsuu@gmail.com>
2026-03-21 00:34:23 +0900

    compression

--- application/server/src/routes/static.ts
+++ application/server/src/routes/static.ts
@@ -1,4 +1,5 @@
 import history from "connect-history-api-fallback";
+import compression from "compression";
 import { Router } from "express";
 import serveStatic from "serve-static";
 
@@ -9,9 +10,19 @@ import {
 } from "@web-speed-hackathon-2026/server/src/paths";
 
 export const staticRouter = Router();
+const commonCompression = compression({ threshold: 0 });
 
 // SPA 対応のため、ファイルが存在しないときに index.html を返す
 staticRouter.use(history());
+
+// SVG のみ gzip 圧縮して配信する
+staticRouter.use((req, res, next) => {
+  if (req.path.endsWith(".svg")) {
+    commonCompression(req, res, next);
+    return;
+  }
+  next();
+});
 
 staticRouter.use(
   serveStatic(UPLOAD_PATH, {
@@ -29,6 +40,9 @@ staticRouter.use(
     },
   }),
 );
+
+// dist 配下は常時 gzip 圧縮して配信する
+staticRouter.use(commonCompression);
 
 staticRouter.use(
   serveStatic(CLIENT_DIST_PATH, {
