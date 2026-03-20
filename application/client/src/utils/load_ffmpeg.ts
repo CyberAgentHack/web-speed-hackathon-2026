0aa1005ff8eacbe2025a0a3b2633dafecc86f557
Shunto Ishiguro <shunto.ishiguro.job@gmail.com>
2026-03-20 18:18:48 +0900

    perf: WASMバイナリをbase64埋め込みからURL参照に変更
    ffmpeg/magick-wasmのWASMをJSチャンクへのbase64埋め込みから
    ?url参照（別ファイル配信）に変更し、JSバンドルサイズを大幅削減

--- application/client/src/utils/load_ffmpeg.ts
+++ application/client/src/utils/load_ffmpeg.ts
@@ -3,14 +3,9 @@ import { FFmpeg } from "@ffmpeg/ffmpeg";
 export async function loadFFmpeg(): Promise<FFmpeg> {
   const ffmpeg = new FFmpeg();
 
-  await ffmpeg.load({
-    coreURL: await import("@ffmpeg/core?binary").then(({ default: b }) => {
-      return URL.createObjectURL(new Blob([b], { type: "text/javascript" }));
-    }),
-    wasmURL: await import("@ffmpeg/core/wasm?binary").then(({ default: b }) => {
-      return URL.createObjectURL(new Blob([b], { type: "application/wasm" }));
-    }),
-  });
+  const coreURL = (await import("@ffmpeg/core?url")).default;
+  const wasmURL = (await import("@ffmpeg/core/wasm?url")).default;
+  await ffmpeg.load({ coreURL, wasmURL });
 
   return ffmpeg;
 }
