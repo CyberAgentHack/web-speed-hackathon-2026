ee72b6c7225afe09ac181faa1782830b7a075ed7
asa1984 <satoasa9913@gmail.com>
2026-03-20 19:36:50 +0900

    Replace native anchor with <Link> of React Router

--- application/client/src/components/foundation/Link.tsx
+++ application/client/src/components/foundation/Link.tsx
@@ -1,13 +1,8 @@
-import { AnchorHTMLAttributes, forwardRef } from "react";
-import { To, useHref } from "react-router";
+import { ComponentPropsWithRef } from "react";
+import { Link as RouterLink } from "react-router";
 
-type Props = Omit<AnchorHTMLAttributes<HTMLAnchorElement>, "href"> & {
-  to: To;
+type Props = ComponentPropsWithRef<typeof RouterLink>;
+
+export const Link = (props: Props) => {
+  return <RouterLink {...props} />;
 };
-
-export const Link = forwardRef<HTMLAnchorElement, Props>(({ to, ...props }, ref) => {
-  const href = useHref(to);
-  return <a ref={ref} href={href} {...props} />;
-});
-
-Link.displayName = "Link";
