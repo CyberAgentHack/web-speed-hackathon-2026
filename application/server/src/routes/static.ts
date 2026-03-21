3cd995336b98f1a83125a2b391ecc79878293624
y-hiraoka <62982380+y-hiraoka@users.noreply.github.com>
2026-03-21 13:09:30 +0900

    Content-Type を調整する

--- application/server/src/routes/static.ts
+++ application/server/src/routes/static.ts
@@ -13,10 +13,17 @@ export const staticRouter = Router();
 // SPA 対応のため、ファイルが存在しないときに index.html を返す
 staticRouter.use(history());
 
+const setMp4ContentType: serveStatic.ServeStaticOptions["setHeaders"] = (res, filePath) => {
+  if (filePath.endsWith(".mp4")) {
+    res.setHeader("Content-Type", "video/mp4");
+  }
+};
+
 staticRouter.use(
   serveStatic(UPLOAD_PATH, {
     etag: false,
     lastModified: false,
+    setHeaders: setMp4ContentType,
   }),
 );
 
@@ -24,6 +31,7 @@ staticRouter.use(
   serveStatic(PUBLIC_PATH, {
     etag: false,
     lastModified: false,
+    setHeaders: setMp4ContentType,
   }),
 );
 
