b43ab09401cb0b6d1981483a9b2e137453aaf622
uni-kakurenbo <kohma1116@gmail.com>
2026-03-21 17:52:54 +0900

    build-time tailwind

--- application/client/vite.config.ts
+++ application/client/vite.config.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import { defineConfig } from "vite";
-import type { Plugin } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import { visualizer } from "rollup-plugin-visualizer";
@@ -8,6 +8,7 @@ import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         react(),
         viteStaticCopy({
             targets: [
