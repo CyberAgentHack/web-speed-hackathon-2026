4b324a6d8f73bc79dc52d3b424fd86201a615d95
Marstach <ryoken.0325@gmail.com>
2026-03-21 17:37:59 +0900

    viteに戻す

--- application/client/src/index.tsx
+++ application/client/src/index.tsx
@@ -1,16 +1,22 @@
+import "./index.css";
+
+import { Buffer } from "buffer";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
+import { HelmetProvider } from "react-helmet";
 import { BrowserRouter } from "react-router";
 
 import { AppContainer } from "@web-speed-hackathon-2026/client/src/containers/AppContainer";
-import { store } from "@web-speed-hackathon-2026/client/src/store";
 
-window.addEventListener("load", () => {
-  createRoot(document.getElementById("app")!).render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <AppContainer />
-      </BrowserRouter>
-    </Provider>,
-  );
-});
+(globalThis as typeof globalThis & { Buffer: typeof Buffer }).Buffer = Buffer;
+
+const root = document.getElementById("app")!;
+// Remove loading indicator injected in HTML
+document.getElementById("app-loading")?.remove();
+
+createRoot(root).render(
+  <HelmetProvider>
+    <BrowserRouter>
+      <AppContainer />
+    </BrowserRouter>
+  </HelmetProvider>,
+);
--- application/client/src/utils/convert_image.ts
+++ application/client/src/utils/convert_image.ts
@@ -1,42 +1,67 @@
-import { initializeImageMagick, ImageMagick, MagickFormat } from "@imagemagick/magick-wasm";
-import magickWasm from "@imagemagick/magick-wasm/magick.wasm?binary";
-import { dump, insert, ImageIFD } from "piexifjs";
+export type ImageFormat = "jpg" | "png" | "gif";
 
 interface Options {
-  extension: MagickFormat;
+  extension: ImageFormat;
 }
 
 export async function convertImage(file: File, options: Options): Promise<Blob> {
+  const [
+    { initializeImageMagick, ImageMagick, MagickFormat },
+    { default: magickWasm },
+    { dump, insert, ImageIFD },
+  ] = await Promise.all([
+    import("@imagemagick/magick-wasm"),
+    import("@imagemagick/magick-wasm/magick.wasm?url"),
+    import("piexifjs"),
+  ]);
+
+  const fmt =
+    options.extension === "jpg" ? MagickFormat.Jpg
+    : options.extension === "png" ? MagickFormat.Png
+    : MagickFormat.Gif;
+
   await initializeImageMagick(magickWasm);
 
   const byteArray = new Uint8Array(await file.arrayBuffer());
 
-  return new Promise((resolve) => {
-    ImageMagick.read(byteArray, (img) => {
-      img.format = options.extension;
+  return new Promise((resolve, reject) => {
+    try {
+      ImageMagick.read(byteArray, (img) => {
+        try {
+          img.format = fmt;
 
-      const comment = img.comment;
+          const comment = img.comment ?? img.getAttribute("exif:ImageDescription");
 
-      img.write((output) => {
-        if (comment == null) {
-          resolve(new Blob([output as Uint8Array<ArrayBuffer>]));
-          return;
+          img.write((output) => {
+            try {
+              if (comment == null) {
+                resolve(new Blob([output as Uint8Array<ArrayBuffer>]));
+                return;
+              }
+
+              // ImageMagick では EXIF の ImageDescription フィールドに保存されているデータが
+              // 非標準の Comment フィールドに移されてしまうため
+              // piexifjs を使って ImageDescription フィールドに書き込む
+              const binary = Array.from(output as Uint8Array<ArrayBuffer>)
+                .map((b) => String.fromCharCode(b))
+                .join("");
+              const descriptionBinary = Array.from(new TextEncoder().encode(comment))
+                .map((b) => String.fromCharCode(b))
+                .join("");
+              const exifStr = dump({ "0th": { [ImageIFD.ImageDescription]: descriptionBinary } });
+              const outputWithExif = insert(exifStr, binary);
+              const bytes = Uint8Array.from(outputWithExif.split("").map((c) => c.charCodeAt(0)));
+              resolve(new Blob([bytes]));
+            } catch (e) {
+              reject(e);
+            }
+          });
+        } catch (e) {
+          reject(e);
         }
-
-        // ImageMagick では EXIF の ImageDescription フィールドに保存されているデータが
-        // 非標準の Comment フィールドに移されてしまうため
-        // piexifjs を使って ImageDescription フィールドに書き込む
-        const binary = Array.from(output as Uint8Array<ArrayBuffer>)
-          .map((b) => String.fromCharCode(b))
-          .join("");
-        const descriptionBinary = Array.from(new TextEncoder().encode(comment))
-          .map((b) => String.fromCharCode(b))
-          .join("");
-        const exifStr = dump({ "0th": { [ImageIFD.ImageDescription]: descriptionBinary } });
-        const outputWithExif = insert(exifStr, binary);
-        const bytes = Uint8Array.from(outputWithExif.split("").map((c) => c.charCodeAt(0)));
-        resolve(new Blob([bytes]));
       });
-    });
+    } catch (e) {
+      reject(e);
+    }
   });
 }
