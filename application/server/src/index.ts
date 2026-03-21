424e4d84ada3d26c6ad7495531829c70b2bd7ad9
otsuboa <assanuts128+github@gmail.com>
2026-03-21 10:35:05 +0900

    feat: implement image alt text population from EXIF data

--- application/server/src/index.ts
+++ application/server/src/index.ts
@@ -1,10 +1,33 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
 import "@web-speed-hackathon-2026/server/src/utils/express_websocket_support";
 import { app } from "@web-speed-hackathon-2026/server/src/app";
+import { Image } from "@web-speed-hackathon-2026/server/src/models";
+import { PUBLIC_PATH } from "@web-speed-hackathon-2026/server/src/paths";
+import { extractExifDescription } from "@web-speed-hackathon-2026/server/src/utils/extract_exif_description";
 
 import { initializeSequelize } from "./sequelize";
 
+async function populateImageAlt() {
+  const images = await Image.findAll({ attributes: ["id"] });
+  for (const image of images) {
+    const jpegPath = path.resolve(PUBLIC_PATH, "images", `${image.id}.jpg`);
+    try {
+      const buf = await fs.readFile(jpegPath);
+      const alt = extractExifDescription(buf);
+      if (alt) {
+        await image.update({ alt });
+      }
+    } catch {
+      // ファイルが存在しない場合はスキップ
+    }
+  }
+}
+
 async function main() {
   await initializeSequelize();
+  await populateImageAlt();
 
   const server = app.listen(Number(process.env["PORT"] || 3000), "0.0.0.0", () => {
     const address = server.address();
