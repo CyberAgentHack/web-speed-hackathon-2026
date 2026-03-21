01a207cf35d471fedeae0358cfc3b0f8f47cd1b6
sayn0s <ssyouth0717@gmail.com>
2026-03-21 12:00:29 +0900

    revert: CoveredImage/TimelineItem のlazy除去を差し戻し（LCP悪化のため）

--- application/client/src/components/foundation/CoveredImage.tsx
+++ application/client/src/components/foundation/CoveredImage.tsx
@@ -23,6 +23,7 @@ export const CoveredImage = ({ alt, src }: Props) => {
       <img
         alt={alt}
         className="absolute inset-0 h-full w-full object-cover"
+        loading="lazy"
         src={src}
       />
 
