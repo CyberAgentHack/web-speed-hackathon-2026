f88f118fe99b6f84d07750ad7cd37e598c412797
asa1984 <satoasa9913@gmail.com>
2026-03-21 17:44:55 +0900

    Lazy importing modal

--- application/client/src/containers/DirectMessageListContainer.tsx
+++ application/client/src/containers/DirectMessageListContainer.tsx
@@ -1,27 +1,33 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { lazy, Suspense } from "react";
 
 import { getMeQueryOptions } from "@web-speed-hackathon-2026/client/src/auth/hooks";
 import { DirectMessageGate } from "@web-speed-hackathon-2026/client/src/components/direct_message/DirectMessageGate";
 import { DirectMessageListPage } from "@web-speed-hackathon-2026/client/src/components/direct_message/DirectMessageListPage";
-import { NewDirectMessageModalContainer } from "@web-speed-hackathon-2026/client/src/containers/NewDirectMessageModalContainer";
 import { MODAL_IDS } from "@web-speed-hackathon-2026/client/src/constants";
+
+const NewDirectMessageModalContainer = lazy(() =>
+  import("@web-speed-hackathon-2026/client/src/containers/NewDirectMessageModalContainer").then(
+    (m) => ({
+      default: m.NewDirectMessageModalContainer,
+    }),
+  ),
+);
 
 export const DirectMessageListContainer = () => {
   const { data: activeUser } = useSuspenseQuery(getMeQueryOptions());
 
   if (activeUser === null) {
-    return (
-      <DirectMessageGate
-        headline="DMを利用するにはサインインが必要です"
-      />
-    );
+    return <DirectMessageGate headline="DMを利用するにはサインインが必要です" />;
   }
 
   return (
     <>
       <title>ダイレクトメッセージ - CaX</title>
       <DirectMessageListPage activeUser={activeUser} newDmModalId={MODAL_IDS.NEW_DM} />
-      <NewDirectMessageModalContainer id={MODAL_IDS.NEW_DM} />
+      <Suspense>
+        <NewDirectMessageModalContainer id={MODAL_IDS.NEW_DM} />
+      </Suspense>
     </>
   );
 };
