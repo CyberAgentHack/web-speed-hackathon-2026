74084a3df89bce96ec49116a9c3c7d3eb8a2701a
tkhs0813 <takahashiry.0813@gmail.com>
2026-03-21 13:39:04 +0900

    Connection: close ヘッダー除去

--- application/server/src/app.ts
+++ application/server/src/app.ts
@@ -18,7 +18,6 @@ app.use(bodyParser.raw({ limit: "10mb" }));
 app.use("/api/v1", (_req, res, next) => {
   res.header({
     "Cache-Control": "max-age=0, no-transform",
-    Connection: "close",
   });
   return next();
 }, apiRouter);
