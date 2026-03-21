a480fe8f2ce5be1b56257d1ad1eb364007cf227e
kutsushisouseki-prog <kutsushisouseki@gmail.com>
2026-03-21 11:58:15 +0900

    Implement loading state in TimelineContainer
    Added loading state handling for initial load in TimelineContainer.

--- application/client/src/containers/TimelineContainer.tsx
+++ application/client/src/containers/TimelineContainer.tsx
@@ -3,10 +3,20 @@ import { Helmet } from "react-helmet";
 import { InfiniteScroll } from "@web-speed-hackathon-2026/client/src/components/foundation/InfiniteScroll";
 import { TimelinePage } from "@web-speed-hackathon-2026/client/src/components/timeline/TimelinePage";
 import { useInfiniteFetch } from "@web-speed-hackathon-2026/client/src/hooks/use_infinite_fetch";
-import { fetchJSON } from "@web-speed-hackathon-2026/client/src/utils/fetchers";
 
 export const TimelineContainer = () => {
-  const { data: posts, fetchMore } = useInfiniteFetch<Models.Post>("/api/v1/posts", fetchJSON);
+  const { data: posts, fetchMore, loading } = useInfiniteFetch<Models.Post>(
+    "/api/v1/posts"
+  );
+
+  // 初回ロード中
+  if (loading && posts.length === 0) {
+    return (
+      <Helmet>
+        <title>読込中 - CaX</title>
+      </Helmet>
+    );
+  }
 
   return (
     <InfiniteScroll fetchMore={fetchMore} items={posts}>
