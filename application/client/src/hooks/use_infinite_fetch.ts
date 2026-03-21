d974f2a3d609291fa3a7d9a368aef453a5466c95
tkura37 <takuto.nagakura@gmail.com>
2026-03-21 18:09:38 +0900

    投稿を最大10件まで取得するよう変更

--- application/client/src/hooks/use_infinite_fetch.ts
+++ application/client/src/hooks/use_infinite_fetch.ts
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 
-const LIMIT = 30;
+const LIMIT = 10;
 
 interface ReturnValues<T> {
   data: Array<T>;
@@ -36,11 +36,12 @@ export function useInfiniteFetch<T>(
       offset,
     };
 
-    void fetcher(apiPath).then(
-      (allData) => {
+    const separator = apiPath.includes("?") ? "&" : "?";
+    void fetcher(`${apiPath}${separator}limit=${LIMIT}&offset=${offset}`).then(
+      (data) => {
         setResult((cur) => ({
           ...cur,
-          data: [...cur.data, ...allData.slice(offset, offset + LIMIT)],
+          data: [...cur.data, ...data],
           isLoading: false,
         }));
         internalRef.current = {
