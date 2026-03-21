8292bc46cafb0ac3245c5837f6f599dc96c7bbf0
cp-20 <n.naoki.1587@gmail.com>
2026-03-21 10:29:00 +0900

    FontAweSome ましに

--- application/client/src/components/foundation/FontAwesomeIcon.tsx
+++ application/client/src/components/foundation/FontAwesomeIcon.tsx
@@ -1,12 +1,83 @@
+import type { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import {
+  faArrowDown,
+  faArrowRight,
+  faBalanceScale,
+  faCircleNotch,
+  faEdit,
+  faEnvelope,
+  faExclamationCircle,
+  faHome,
+  faImages,
+  faMusic,
+  faPaperPlane,
+  faPause,
+  faPlay,
+  faSearch,
+  faSignInAlt,
+  faUser,
+  faVideo,
+} from "@fortawesome/free-solid-svg-icons";
+import { faCalendarAlt } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon as ReactFontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+type StyleType = "solid" | "regular";
+type SolidIconType =
+  | "home"
+  | "search"
+  | "envelope"
+  | "edit"
+  | "user"
+  | "sign-in-alt"
+  | "balance-scale"
+  | "arrow-right"
+  | "paper-plane"
+  | "circle-notch"
+  | "images"
+  | "music"
+  | "video"
+  | "exclamation-circle"
+  | "arrow-down"
+  | "play"
+  | "pause";
+type RegularIconType = "calendar-alt";
+type IconType = SolidIconType | RegularIconType;
+
 interface Props {
-  iconType: string;
-  styleType: "solid" | "regular";
+  iconType: IconType;
+  styleType: StyleType;
 }
 
+const solidIcons: Record<SolidIconType, IconDefinition> = {
+  home: faHome,
+  search: faSearch,
+  envelope: faEnvelope,
+  edit: faEdit,
+  user: faUser,
+  "sign-in-alt": faSignInAlt,
+  "balance-scale": faBalanceScale,
+  "arrow-right": faArrowRight,
+  "paper-plane": faPaperPlane,
+  "circle-notch": faCircleNotch,
+  images: faImages,
+  music: faMusic,
+  video: faVideo,
+  "exclamation-circle": faExclamationCircle,
+  "arrow-down": faArrowDown,
+  play: faPlay,
+  pause: faPause,
+};
+
+const regularIcons: Record<RegularIconType, IconDefinition> = {
+  "calendar-alt": faCalendarAlt,
+};
+
 export const FontAwesomeIcon = ({ iconType, styleType }: Props) => {
-  return (
-    <svg className="font-awesome inline-block fill-current leading-none">
-      <use xlinkHref={`/sprites/font-awesome/${styleType}.svg#${iconType}`} />
-    </svg>
-  );
+  const icon = styleType === "solid" ? solidIcons[iconType as SolidIconType] : regularIcons[iconType as RegularIconType];
+
+  if (!icon) {
+    return null;
+  }
+
+  return <ReactFontAwesomeIcon icon={icon} className="font-awesome inline-block fill-current leading-none" />;
 };
