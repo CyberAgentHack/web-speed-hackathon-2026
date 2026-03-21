ddd3fdee079670adbd0749ac0baa2a0540f4cdb0
kure-t <tatuya.kure@gmail.com>
2026-03-21 13:31:51 +0900

    重いコンポーネントのlazy化

--- application/client/src/containers/AppContainer.tsx
+++ application/client/src/containers/AppContainer.tsx
@@ -1,12 +1,12 @@
-import { useCallback, useEffect, useId, useState } from "react";
+import { Suspense, lazy, useCallback, useEffect, useId, useState } from "react";
 import { HelmetProvider } from "react-helmet";
 import { Route, Routes, useLocation, useNavigate } from "react-router";
 
 import { AppPage } from "@web-speed-hackathon-2026/client/src/components/application/AppPage";
 import { AuthModalContainer } from "@web-speed-hackathon-2026/client/src/containers/AuthModalContainer";
-import { CrokContainer } from "@web-speed-hackathon-2026/client/src/containers/CrokContainer";
-import { DirectMessageContainer } from "@web-speed-hackathon-2026/client/src/containers/DirectMessageContainer";
-import { DirectMessageListContainer } from "@web-speed-hackathon-2026/client/src/containers/DirectMessageListContainer";
+// import { CrokContainer } from "@web-speed-hackathon-2026/client/src/containers/CrokContainer";
+// import { DirectMessageContainer } from "@web-speed-hackathon-2026/client/src/containers/DirectMessageContainer";
+// import { DirectMessageListContainer } from "@web-speed-hackathon-2026/client/src/containers/DirectMessageListContainer";
 import { NewPostModalContainer } from "@web-speed-hackathon-2026/client/src/containers/NewPostModalContainer";
 import { NotFoundContainer } from "@web-speed-hackathon-2026/client/src/containers/NotFoundContainer";
 import { PostContainer } from "@web-speed-hackathon-2026/client/src/containers/PostContainer";
@@ -15,6 +15,23 @@ import { TermContainer } from "@web-speed-hackathon-2026/client/src/containers/T
 import { TimelineContainer } from "@web-speed-hackathon-2026/client/src/containers/TimelineContainer";
 import { UserProfileContainer } from "@web-speed-hackathon-2026/client/src/containers/UserProfileContainer";
 import { fetchJSON, sendJSON } from "@web-speed-hackathon-2026/client/src/utils/fetchers";
+
+const CrokContainer = lazy(async () => {
+  const module = await import("@web-speed-hackathon-2026/client/src/containers/CrokContainer");
+  return { default: module.CrokContainer };
+});
+const DirectMessageContainer = lazy(async () => {
+  const module = await import(
+    "@web-speed-hackathon-2026/client/src/containers/DirectMessageContainer"
+  );
+  return { default: module.DirectMessageContainer };
+});
+const DirectMessageListContainer = lazy(async () => {
+  const module = await import(
+    "@web-speed-hackathon-2026/client/src/containers/DirectMessageListContainer"
+  );
+  return { default: module.DirectMessageListContainer };
+});
 
 export const AppContainer = () => {
   const { pathname } = useLocation();
@@ -54,12 +71,18 @@ export const AppContainer = () => {
           <Route element={<TimelineContainer />} path="/" />
           <Route
             element={
-              <DirectMessageListContainer activeUser={activeUser} authModalId={authModalId} />
+              <Suspense fallback={null}>
+                <DirectMessageListContainer activeUser={activeUser} authModalId={authModalId} />
+              </Suspense>
             }
             path="/dm"
           />
           <Route
-            element={<DirectMessageContainer activeUser={activeUser} authModalId={authModalId} />}
+            element={
+              <Suspense fallback={null}>
+                <DirectMessageContainer activeUser={activeUser} authModalId={authModalId} />
+              </Suspense>
+            }
             path="/dm/:conversationId"
           />
           <Route element={<SearchContainer />} path="/search" />
@@ -67,7 +90,11 @@ export const AppContainer = () => {
           <Route element={<PostContainer />} path="/posts/:postId" />
           <Route element={<TermContainer />} path="/terms" />
           <Route
-            element={<CrokContainer activeUser={activeUser} authModalId={authModalId} />}
+            element={
+              <Suspense fallback={null}>
+                <CrokContainer activeUser={activeUser} authModalId={authModalId} />
+              </Suspense>
+            }
             path="/crok"
           />
           <Route element={<NotFoundContainer />} path="*" />
