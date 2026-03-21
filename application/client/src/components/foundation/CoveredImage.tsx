7e51c1ca6c5a0e096a7bbc52ab9e7155112e878c
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-03-21 13:33:34 +0900

    Fix CoveredImage shift

--- application/client/src/components/foundation/CoveredImage.tsx
+++ application/client/src/components/foundation/CoveredImage.tsx
@@ -34,8 +34,7 @@ export const CoveredImage = ({ src }: Props) => {
 
   return (
     <div className="relative h-full w-full overflow-hidden">
-      <img alt={alt} className={"object-cover"} src={src} />
-
+      <img alt={alt} className={"absolute w-full h-full object-cover"} src={src} />
       <button
         className="border-cax-border bg-cax-surface-raised/90 text-cax-text-muted hover:bg-cax-surface absolute right-1 bottom-1 rounded-full border px-2 py-1 text-center text-xs"
         type="button"
