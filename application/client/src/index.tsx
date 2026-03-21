6f01ff34bb06fcb7b8d94cfa6ffa9776854d11df
kutsushisouseki-prog <kutsushisouseki@gmail.com>
2026-03-21 16:14:24 +0900

    Update index.tsx

--- application/client/src/index.tsx
+++ application/client/src/index.tsx
@@ -1,17 +1,19 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router";
+import { Suspense } from "react";
+import { AppContainer } from "./containers/AppContainer";
+import { store } from "./store";
 
-import { AppContainer } from "@web-speed-hackathon-2026/client/src/containers/AppContainer";
-import { store } from "@web-speed-hackathon-2026/client/src/store";
-
- const container = document.getElementById("app");
- if (container) {
-   createRoot(container).render(
-        <Provider store={store}>
-          <BrowserRouter>
-            <AppContainer />
-          </BrowserRouter>
-        </Provider>
-      );
-    }
+const container = document.getElementById("app");
+if (container) {
+  createRoot(container).render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <Suspense fallback={<div>起動中...</div>}>
+          <AppContainer />
+        </Suspense>
+      </BrowserRouter>
+    </Provider>
+  );
+}
