23dc463016132000316e460060faae112bad8495
Ryoga Yoshdia <ryoga.yoshida@worklog-inc.com>
2026-03-21 15:56:43 +0900

    通信を圧縮する

--- application/server/src/routes/static.ts
+++ application/server/src/routes/static.ts
@@ -1,4 +1,5 @@
 import history from "connect-history-api-fallback";
+import compression from "compression";
 import { Router } from "express";
 import serveStatic from "serve-static";
 
@@ -12,9 +13,11 @@ const IMMUTABLE_CACHE_CONTROL = "public, max-age=31536000, immutable";
 const SHORT_CACHE_CONTROL = "public, max-age=3600";
 
 export const staticRouter = Router();
+const compress = compression({ threshold: 0 });
 
 // SPA 対応のため、ファイルが存在しないときに index.html を返す
 staticRouter.use(history());
+staticRouter.use(compress);
 
 staticRouter.use(
   serveStatic(UPLOAD_PATH, {
