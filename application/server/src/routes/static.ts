c1ad907aada021b679f4aac4f680761dea000f4a
Luke256 <yuta.k.200628@gmail.com>
2026-03-21 10:50:02 +0900

    キャッシュを取るように

--- application/server/src/routes/static.ts
+++ application/server/src/routes/static.ts
@@ -10,6 +10,10 @@ import {
 
 export const staticRouter = Router();
 
+const setStaticCacheHeaders = (res: { setHeader: (name: string, value: string) => void }): void => {
+  res.setHeader("Cache-Control", "public, max-age=300, immutable");
+};
+
 // SPA 対応のため、ファイルが存在しないときに index.html を返す
 staticRouter.use(history());
 
@@ -17,6 +21,7 @@ staticRouter.use(
   serveStatic(UPLOAD_PATH, {
     etag: false,
     lastModified: false,
+    setHeaders: setStaticCacheHeaders,
   }),
 );
 
@@ -24,6 +29,7 @@ staticRouter.use(
   serveStatic(PUBLIC_PATH, {
     etag: false,
     lastModified: false,
+    setHeaders: setStaticCacheHeaders,
   }),
 );
 
@@ -31,5 +37,6 @@ staticRouter.use(
   serveStatic(CLIENT_DIST_PATH, {
     etag: false,
     lastModified: false,
+    setHeaders: setStaticCacheHeaders,
   }),
 );
