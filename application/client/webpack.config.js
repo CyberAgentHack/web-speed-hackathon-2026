8a312220442241f6e611687e78e3169b6adc197d
kutsushisouseki-prog <kutsushisouseki@gmail.com>
2026-03-21 18:10:48 +0900

    Update webpack.config.js

--- application/client/webpack.config.js
+++ application/client/webpack.config.js
@@ -7,6 +7,7 @@ const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
   mode: isProduction ? "production" : "development",
+  devtool: false,
   entry: ["core-js/stable", "regenerator-runtime/runtime", "./src/index.tsx"],
   output: {
     path: path.resolve(__dirname, "../dist"),
@@ -25,15 +26,12 @@ module.exports = {
     extensions: [".tsx", ".ts", ".js"],
     alias: {
       "kuromoji$": path.resolve(__dirname, "node_modules/kuromoji/build/kuromoji.js"),
-      "@ffmpeg/ffmpeg$": path.resolve(__dirname, "node_modules/@ffmpeg/ffmpeg/dist/esm/index.js"),
-      "@ffmpeg/core$": path.resolve(__dirname, "node_modules/@ffmpeg/core/dist/umd/ffmpeg-core.js"),
-      "@imagemagick/magick-wasm/magick.wasm$": path.resolve(__dirname, "node_modules/@imagemagick/magick-wasm/dist/magick.wasm"),
     },
     fallback: {
       fs: false,
       path: false,
       url: false,
-      process: require.resolve("process/browser"),
+      process: require.resolve("process/browser.js"),
       buffer: require.resolve("buffer/"),
     },
   },
@@ -41,11 +39,8 @@ module.exports = {
     new MiniCssExtractPlugin({ filename: "c/[name].css" }),
     new HtmlWebpackPlugin({ template: "./src/index.html" }),
     new webpack.ProvidePlugin({
-      process: "process/browser",
+      process: "process/browser.js",
       Buffer: ["buffer", "Buffer"],
-    }),
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(isProduction ? "production" : "development"),
     }),
   ],
   optimization: { minimize: isProduction },
